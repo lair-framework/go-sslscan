683d4c0c4b1bf74d73bf40156ac80488628c5626
mattburch <mburch44@gmail.com>
2015-11-07 14:25:47 -0500

    Update naming convention

--- sslscan.go
+++ sslscan.go
@@ -6,13 +6,15 @@ import (
 	"encoding/xml"
 )
 
-type SSLScanRun struct {
+// SSLScanData contains all the data form a single sslscan job.
+type SSLScanData struct {
 	Title   string    `xml:"title,attr"`
 	Version string    `xml:"version,attr"`
 	Web     string    `xml:"web,attr"`
 	SSLTest []SSLTest `xml:"ssltest"`
 }
 
+// SSLTest contains sslscan results.
 type SSLTest struct {
 	Host          string        `xml:"host,attr"`
 	Port          string        `xml:"port,attr"`
@@ -24,20 +26,24 @@ type SSLTest struct {
 	Certificate   Certificate   `xml:"certificate"`
 }
 
+// Renegotation contains the renegotation results.
 type Renegotiation struct {
 	Supported bool `xml:"supported,attr"`
 	Secure    bool `xml:"secure,attr"`
 }
 
+// Compression contains the compression results.
 type Compression struct {
 	Supported bool `xml:"supported,attr"`
 }
 
+// Heatbleed contains the heartbleed results.
 type Heartbleed struct {
 	SSLVersion string `xml:"sslversion,attr"`
 	Vulnerable bool   `xml:"vulnerable,attr"`
 }
 
+// Cipher contains the support cipher suites..
 type Cipher struct {
 	Status     string `xml:"status,attr"`
 	SSLVersion string `xml:"sslversion,attr"`
@@ -45,6 +51,7 @@ type Cipher struct {
 	Cipher     string `xml:"cipher,attr"`
 }
 
+// Certificate contains the certificate parameters.
 type Certificate struct {
 	SignatureAlgorithm string    `xml:"signature-algorithm"`
 	PublicKey          PublicKey `xml:"pk"`
@@ -54,6 +61,7 @@ type Certificate struct {
 	SelfSigned         bool      `xml:"self-signed"`
 }
 
+// PublicKey contains the public key parameters.
 type PublicKey struct {
 	Error bool   `xml:"error,attr"`
 	Type  string `xml:"type,attr"`
@@ -63,8 +71,8 @@ type PublicKey struct {
 // Parse takes a byte array of sslscan xml data and unmarshals it into an
 // SSLScanRun struct. All elements are returned as strings, it is up to the caller
 // to check and cast them to the proper type.
-func Parse(content []byte) (*SSLScanRun, error) {
-	r := &SSLScanRun{}
+func Parse(content []byte) (*SSLScanData, error) {
+	r := &SSLScanData{}
 	err := xml.Unmarshal(content, r)
 	if err != nil {
 		return r, err
